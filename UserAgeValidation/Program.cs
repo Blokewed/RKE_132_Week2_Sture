0ebb54e1faf2680bd978a8b713f90c9fbdb25827
Blokewed <peeter.sture@tktk.ee>
2024-09-10 23:06:20 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Please enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum}.");
+
+if (isAgeNumber)
+{
+    if (userAgeNum >=13)
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram");
+    }
+}
+
+else
+{
+    Console.WriteLine("Could not verify your age.");
+}
+
+
+
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Please select your gender (m/f):");
+
+char userGender = char.Parse(Console.ReadLine());
+Console.WriteLine("Please, enter your name:");
+
+string userName = Console.ReadLine();
+
+
+if (userGender == 'm') 
+{
+    Console.WriteLine($"Welcome, Mr {userName}!");
+}
+
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms {userName}!");
+}
+
+else
+{
+    Console.WriteLine($"Welcome {userName}");
+}
